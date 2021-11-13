cac7c747bee9f35c4c61670db16276056a386c85
Gabriel <Gabrielabdul1@gmail.com>
2021-11-13 07:42:16 +0000

    allocates memory to store a graph, returns graph or NULL on fail

--- 0x01-graphs/0-graph_create.c
+++ 0x01-graphs/0-graph_create.c
@@ -1,11 +1,22 @@
 #include "graphs.h"
 
 /**
- * graph_create -
- *
- * Return: 
+ * graph_create - function that allocates memory to store
+ * a graph_t structure, and initializes its content.
+ * Return: allocated graph, or NULL on failure
  */
 graph_t *graph_create(void)
 {
+    graph_t *graph;
+
+    graph = malloc(sizeof(graph_t));
+
+    if (!graph)
+        return (NULL);
+    
+    graph->nb_vertices = 0;
+    graph->vertices = NULL;
+
+    return (graph);
 
 }

75ee7e77f3b85aed80418298a796282799be7387
Gabriel <Gabrielabdul1@gmail.com>
2021-11-16 19:59:44 +0000

    function that frees a graph and all allocated members from memory

--- 0x01-graphs/3-graph_delete.c
+++ 0x01-graphs/3-graph_delete.c
@@ -1,11 +1,41 @@
 #include "graphs.h"
 
 /**
- * graph_delete -
- *
- * Return: 
+ * graph_delete - frees all allocated space used for graph and members
+ * @graph: graph to delete
  */
 void graph_delete(graph_t *graph)
 {
+    vertex_t *v, tmp;
+    edge_t *e, *next;
 
+    if (graph == NULL)
+        exit(EXIT_FAILURE);
+
+    if (graph->vertices == NULL)
+        free(graph);
+    else
+    {
+        v = graph->verticies;
+        while (v)
+        {
+            if (v->edges)
+            {
+                e = v->edges;
+                while (e)
+                {
+                    if (e->next)
+                        tmp = e->next;
+                    free(e)
+                    e = tmp;
+                }
+            }
+            free(v->content);
+            free(v)
+        }
+        free(graph->vertices);
+    }
+    free(graph);
+
+        
 }

0daeea463dfc050ba47e433a248413d65da34865
Gabriel <Gabrielabdul1@gmail.com>
2021-11-16 20:00:23 +0000

    frees graph and members from memory -- kernel style coding fix

--- 0x01-graphs/3-graph_delete.c
+++ 0x01-graphs/3-graph_delete.c
@@ -6,36 +6,34 @@
  */
 void graph_delete(graph_t *graph)
 {
-    vertex_t *v, tmp;
-    edge_t *e, *next;
+	vertex_t *v, tmp;
+	edge_t *e, *next;
 
-    if (graph == NULL)
-        exit(EXIT_FAILURE);
+	if (graph == NULL)
+		exit(EXIT_FAILURE);
 
-    if (graph->vertices == NULL)
-        free(graph);
-    else
-    {
-        v = graph->verticies;
-        while (v)
-        {
-            if (v->edges)
-            {
-                e = v->edges;
-                while (e)
-                {
-                    if (e->next)
-                        tmp = e->next;
-                    free(e)
-                    e = tmp;
-                }
-            }
-            free(v->content);
-            free(v)
-        }
-        free(graph->vertices);
-    }
-    free(graph);
-
-        
+	if (graph->vertices == NULL)
+		free(graph);
+	else
+	{
+		v = graph->verticies;
+		while (v)
+		{
+			if (v->edges)
+			{
+				e = v->edges;
+				while (e)
+				{
+					if (e->next)
+						tmp = e->next;
+					free(e)
+					e = tmp;
+				}
+			}
+			free(v->content);
+			free(v)
+		}
+		free(graph->vertices);
+	}
+	free(graph);
 }

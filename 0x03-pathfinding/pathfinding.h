85dfb48fdb8cf8e87ceb71d4d272e7c6fdaa477a
Gabriel <Gabrielabdul1@gmail.com>
2022-01-06 15:45:09 +0000

    updated header file with dijkstra task

--- 0x03-pathfinding/pathfinding.h
+++ 0x03-pathfinding/pathfinding.h
@@ -1,6 +1,21 @@
 #ifndef __PATHFINDING_H__
 #define __PATHFINDING_H__
 
+#define RIGHT (x + 1)
+#define BOTTOM (y + 1)
+#define LEFT (x - 1)
+#define TOP (y - 1)
+#define STRDUP(x) ((str = strdup(x)) ? str : (exit(1), NULL))
+#define ALLOCATE_ALL \
+	do { \
+		dists = calloc(graph->nb_vertices, sizeof(*dists)); \
+		fists = calloc(graph->nb_vertices, sizeof(*fists)); \
+		from = calloc(graph->nb_vertices, sizeof(*from)); \
+		verts = calloc(graph->nb_vertices, sizeof(*verts)); \
+	} while (0)
+#define H(a) ((int)sqrt(pow((a->x - target->x), 2) + \
+	pow((a->y - target->y), 2)))
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -27,5 +42,7 @@ int backtrack(char **map, int rows, int cols, point_t const *target,
 queue_t *backtracking_array(char **map, int rows, int cols,
 							point_t const *start, point_t const *target);
 queue_t *backtracking_graph(graph_t *graph, vertex_t const *start,
+		vertex_t const *target);
+queue_t *dijkstra_graph(graph_t *graph, vertex_t const *start,
 		vertex_t const *target);
 #endif /* __PATHFINDING_H__ */

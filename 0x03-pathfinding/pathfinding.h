d25b3adfc1f216cecf5b5d857b26b00650e1166e
Gabriel <Gabrielabdul1@gmail.com>
2022-01-06 15:16:46 +0000

    updated header file

--- 0x03-pathfinding/pathfinding.h
+++ 0x03-pathfinding/pathfinding.h
@@ -1,11 +1,30 @@
-#ifndef PATHFINDING_H
-#define PATHFINDING_H
+#ifndef __PATHFINDING_H__
+#define __PATHFINDING_H__
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <limits.h>
+#include <math.h>
+#include "graphs.h"
+#include "queues.h"
 
-queue_t *backtracking_array(char **map, int rows, int cols, point_t const *start, point_t const *target);
-queue_t *backtracking_graph(graph_t *graph, vertex_t const *start, vertex_t const *target);
-queue_t *dijkstra_graph(graph_t *graph, vertex_t const *start, vertex_t const *target);
+/**
+ * struct point_s - Structure storing coordinates
+ *
+ * @x: X coordinate
+ * @y: Y coordinate
+ */
+typedef struct point_s
+{
+	int x;
+	int y;
+} point_t;
 
-#endif /* PATHFINDING_H */
+int backtrack(char **map, int rows, int cols, point_t const *target,
+	int x, int y, queue_t *path);
+queue_t *backtracking_array(char **map, int rows, int cols,
+							point_t const *start, point_t const *target);
+
+#endif /* __PATHFINDING_H__ */

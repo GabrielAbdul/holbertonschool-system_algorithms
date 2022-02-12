f953bf0788fd9d5ebc3c9bb8a709b5f2060a774b
Gabriel <Gabrielabdul1@gmail.com>
2022-02-12 18:25:49 +0000

    function that deletes a complete nary tree

---
+++ 0x04-nary_trees/1-nary_tree_delete.c
@@ -0,0 +1,20 @@
+#include "nary_trees.h"
+
+/**
+ * nary_tree_delete - deletes a whoel nary tree
+ * @tree: tree to delete
+ */
+void nary_tree_delete(nary_tree_t *tree)
+{
+
+    nary_tree_t *node = NULL;
+
+    while (tree)
+    {
+        node = tree;
+        tree = tree->next;
+        free(node->content);
+        nary_tree_delete(node->children);
+        free(node);
+    }
+}

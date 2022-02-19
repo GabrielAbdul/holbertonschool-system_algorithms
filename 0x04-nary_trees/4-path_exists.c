08fc640b31dc0a6befa0e186951078d27d30a52f
Gabriel <Gabrielabdul1@gmail.com>
2022-02-19 16:53:11 +0000

    kernel style coding fixes

--- 0x04-nary_trees/4-path_exists.c
+++ 0x04-nary_trees/4-path_exists.c
@@ -8,26 +8,26 @@
  */
 int path_exists(nary_tree_t const *root, char const * const *path)
 {
-    int result;
-    nary_tree_t const *node;
+	int result;
+	nary_tree_t const *node;
 
-    /* if root is NULL there is no path */
-    if (root == NULL)
-        return (0);
+	/* if root is NULL there is no path */
+	if (root == NULL)
+		return (0);
 
-    /* loop through level 0 tree nodes */
-    node = root;
-    while (node)
-    {
-        result = path_exists_recursive(node, path, 0);
-        /* if a match isnt found */
-        if (result > 0)
-            /* check the next node */
-            node = node->next;
-        else
-            return (result);
-    }
-    return (1);
+	/* loop through level 0 tree nodes */
+	node = root;
+	while (node)
+	{
+		result = path_exists_recursive(node, path, 0);
+		/* if a match isnt found */
+		if (result > 0)
+			/* check the next node */
+			node = node->next;
+		else
+			return (result);
+	}
+	return (1);
 }
 /**
  * path_exists_recursive - helper that checks if path exists
@@ -36,29 +36,30 @@ int path_exists(nary_tree_t const *root, char const * const *path)
  * @level: level of tree we're at
  * Return: 1 if path exists, else 0
  */
-int path_exists_recursive(nary_tree_t const *root, char const * const *path, int level)
+int path_exists_recursive(nary_tree_t const *root,
+									  char const * const *path,
+									  int level)
 {
-    nary_tree_t const *node;
+	nary_tree_t const *node;
 
-    if (root == NULL)
-        return (0);
+	if (root == NULL)
+		return (0);
 
-    node = root;
-    while (node && path[level])
-    {
-        /* check this nodes content with corresponding path */
-        if (strcmp(node->content, path[level]) == 0)
-        {
-            /* if we found a match, check for the next part in the path */
-            node = node->children;
-            path_exists_recursive(node, path, level++);
-        } else {
-            node = node->next;
-        }
-    }
-    /* if we reached here and path[level] is NULL then we've checked every
-     * part of the path */
-    if (path[level] == NULL)
-        return (1);
-    return (0);
+	node = root;
+	while (node && path[level])
+	{
+		/* check this nodes content with corresponding path */
+		if (strcmp(node->content, path[level]) == 0)
+		{
+			/* if we found a match, check for the next part in the path */
+			node = node->children;
+			path_exists_recursive(node, path, level++);
+		}
+		else
+			node = node->next;
+	}
+	/* if  path[level] is NULL then we've checked every part of the path */
+	if (path[level] == NULL)
+		return (1);
+	return (0);
 }

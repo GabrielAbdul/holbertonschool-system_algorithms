78e614e5e2f3c16f4550a494255552a907cc1208
Gabriel <Gabrielabdul1@gmail.com>
2022-02-19 17:12:03 +0000

    task 1 kernel style coding fix

--- 0x04-nary_trees/1-nary_tree_delete.c
+++ 0x04-nary_trees/1-nary_tree_delete.c
@@ -7,14 +7,14 @@
 void nary_tree_delete(nary_tree_t *tree)
 {
 
-    nary_tree_t *node = NULL;
+	nary_tree_t *node = NULL;
 
-    while (tree)
-    {
-        node = tree;
-        tree = tree->next;
-        free(node->content);
-        nary_tree_delete(node->children);
-        free(node);
-    }
+	while (tree)
+	{
+		node = tree;
+		tree = tree->next;
+		free(node->content);
+		nary_tree_delete(node->children);
+		free(node);
+	}
 }

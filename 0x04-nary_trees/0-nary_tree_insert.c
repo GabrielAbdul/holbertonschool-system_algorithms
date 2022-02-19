91f6b6b9a6c9562f68907c0497dbefc1506470cd
Gabriel <Gabrielabdul1@gmail.com>
2022-02-19 17:11:13 +0000

    task 0 kernel style coding fixes

--- 0x04-nary_trees/0-nary_tree_insert.c
+++ 0x04-nary_trees/0-nary_tree_insert.c
@@ -1,35 +1,35 @@
 #include "nary_trees.h"
 
 /**
- * nary_tree_insert -
+ * nary_tree_insert - inserts node into nary tree
  * @parent: parent to the pointer node
  * @str: string to be stored, must be duped
  * Return: pointer to new node
  */
 nary_tree_t *nary_tree_insert(nary_tree_t *parent, char const *str)
 {
-    nary_tree_t *node;
+	nary_tree_t *node;
 
-    node = malloc(sizeof(nary_tree_t));
+	node = malloc(sizeof(nary_tree_t));
 
-    if (node == NULL)
-        return (NULL);
+	if (node == NULL)
+		return (NULL);
 
-    node->content = strdup(str);
-    node->nb_children = 0;
-    node->children = NULL;
+	node->content = strdup(str);
+	node->nb_children = 0;
+	node->children = NULL;
 
-    if (parent == NULL)
-    {
-        node->parent = NULL;
-        node->next = NULL;
-        return (node);
-    }
+	if (parent == NULL)
+	{
+		node->parent = NULL;
+		node->next = NULL;
+		return (node);
+	}
 
-    node->next = parent->children;
-    node->parent = parent;
-    parent->children = node;
-    parent->nb_children++;
+	node->next = parent->children;
+	node->parent = parent;
+	parent->children = node;
+	parent->nb_children++;
 
-    return node;
+	return (node);
 }

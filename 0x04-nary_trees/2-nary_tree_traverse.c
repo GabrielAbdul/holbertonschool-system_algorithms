6b2846aed0c38379e99ba0a6821d8ffc9ad939ba
Gabriel <Gabrielabdul1@gmail.com>
2022-02-23 05:04:16 +0000

    function that goes through an N-ary tree, node by node

--- 0x04-nary_trees/2-nary_tree_traverse.c
+++ 0x04-nary_trees/2-nary_tree_traverse.c
@@ -1,28 +1,38 @@
 #include "nary_trees.h"
 
+/**
+ * nary_tree_traverse - will traverse an n-ary tree
+ * @root: pointer to root node of tree
+ * @action: action for each node
+ * Return: max depth
+ */
+size_t nary_tree_traverse(nary_tree_t const *root,
+	void (*action)(nary_tree_t const *node, size_t depth))
+{
+	if (root == NULL || action == NULL)
+		return (0);
+		
+	return (helper(root, action, 0));
+}
 
 /**
- * nary_tree_traverse - traverses an nary tree
- * @root: root node of tree to traverse
- * @action: function to perform on each node
- * @node: node being traversed
- * @depth: depth of the node being traversed
- * Return: biggest depth
+ * helper - will traverse an n-ary tree
+ * @root: pointer to root node of tree
+ * @action: action for each node
+ * @depth: current depth
+ * Return: max depth
  */
-size_t nary_tree_traverse(nary_tree_t const *root, void (*action)(nary_tree_t const *node, size_t depth))
+size_t helper(nary_tree_t const *root,
+	void (*action)(nary_tree_t const *node, size_t depth), size_t depth)
 {
-    size_t current_depth = 0, max_depth;
+	size_t max_depth = depth, current_depth;
 
-    if (root == NULL)
-        return (zero_depth);
-
-    while (root)
-    {
-        action(root, current_depth++);
-        max_depth = nary_tree_traverse(root->children, action);
-        root = root->next;
-        if (max_depth > current_depth)
-            current_depth = max_depth;
-    }
-    return (max_depth);
+	if (!root)
+		return (0);
+	action(root, depth);
+	current_depth = helper(root->children, action, depth + 1);
+	max_depth = MAX(max_depth, current_depth);
+	current_depth = helper(root->next, action, depth);
+	max_depth = MAX(max_depth, current_depth);
+	return (max_depth);
 }

defa3d3ba1ea39faa346ea1ca2115d288d800a37
Gabriel <Gabrielabdul1@gmail.com>
2022-02-23 05:09:40 +0000

    initial commit

---
+++ 0x04-nary_trees/3-nary_tree_diameter.c
@@ -0,0 +1,14 @@
+#include "nary_trees.h"
+
+/**
+ * nary_tree_diameter - computes the diameter of an N-ary tree
+ * @root: pointer to root node of tree
+ * Return: diameter
+ */
+size_t nary_tree_diameter(nary_tree_t const *root)
+{
+    if (root == NULL)
+        return (0);
+
+    return (1);
+}
